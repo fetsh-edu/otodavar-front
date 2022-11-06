d4e193f013d1ea784de0cc0e9bbf79b77e36dbd5
fetsh-edu <fetsh.me@gmail.com>
2022-11-06 21:15:34 +0200

    Authorization finished kind of

--- src/Main.elm
+++ src/Main.elm
@@ -10,8 +10,9 @@ import Dict
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick)
-import Http exposing (Error(..))
-import Json.Decode as Decode exposing (field)
+import Http exposing (Error(..), Expect, expectStringResponse)
+import Json.Decode as Decode exposing (at, decodeString, field)
+import Json.Encode as Encode
 import Http
 import Json.Decode as Json
 import Jwt exposing (decodeToken, tokenDecoder, errorToString)
@@ -22,20 +23,43 @@ import Url exposing (Protocol(..), Url)
 import Url.Parser as Url exposing ((<?>))
 import Url.Parser.Query as Query
 
--- ---------------------------
--- PORTS
--- ---------------------------
 
 configuration : Configuration
 configuration =
-    { authorizationEndpoint =
-        { defaultHttpsUrl | host = "accounts.google.com", path = "/o/oauth2/v2/auth" }
-    , userInfoEndpoint =
-        { defaultHttpsUrl | host = "www.googleapis.com", path = "/oauth2/v1/userinfo" }
+    { authorizationEndpoint = { defaultHttpsUrl | host = "accounts.google.com", path = "/o/oauth2/v2/auth" }
+    , userInfoEndpoint = { defaultHttpsUrl | host = "localhost", path = "/jwt", port_ = Just 3001 }
     , clientId = "744236351761-3enuq53j5e76109de883r0uhb7cb9tc1.apps.googleusercontent.com"
+    --, scope = [ "profile" ]
     , scope = [ "profile email" ]
     , responseType = CustomResponse "id_token token"
+    --, responseType = CustomResponse "id_token token"
     }
+
+main : Program Flags Model Msg
+main =
+    application
+        { init = init
+        , update = update
+        , subscriptions = always <| Sub.batch
+            [ randomBytes GotRandomBytes
+            , onSessionChange decodeFromStorage
+            ]
+        , onUrlRequest = always NoOp
+        , onUrlChange = always NoOp
+        , view = view
+        }
+
+
+
+userFromStorageDecoder : Decode.Decoder (Maybe OtoUser)
+userFromStorageDecoder = Decode.oneOf
+    [ Decode.map Just decodeUser
+    , Decode.null Nothing
+    ]
+
+decodeFromStorage : Decode.Value -> Msg
+decodeFromStorage =
+    GotOtoUserFromStorage << Decode.decodeValue userFromStorageDecoder
 
 
 -- ---------------------------
@@ -43,19 +67,21 @@ configuration =
 -- ---------------------------
 
 type alias Model =
-    { redirectUri : Url
+    { rootUrl : Url
     , flow : Flow
     }
 
 type Flow
     = Idle
-    | Authorized OAuth.Token IdToken
-    | Errored Error
+    | GoogleAuthed OAuth.Token IdToken
+    | OtoAuthed OtoUser
+    | Erred Error
 
 type Error
     = ErrStateMismatch
     | ErrAuthorization OAuth.AuthorizationError
     | ErrHTTPGetUserInfo
+    | ErrCacheParse Decode.Error
 
 type alias Configuration =
     { authorizationEndpoint : Url
@@ -73,49 +99,50 @@ type alias Configuration =
     token and other fields as query parameters.
 When query params are present (and valid), we consider the user `Authorized`.
 -}
-init : Maybe { state : String } -> Url -> Key -> ( Model, Cmd Msg )
-init mflags origin navigationKey =
-    let
-        do = Debug.log "Origin: " origin
-        redirectUri =
-            { origin | query = Nothing, fragment = Nothing }
 
-        clearUrl =
-            Navigation.replaceUrl navigationKey (Url.toString redirectUri)
+type alias Flags =
+  { bytes : Maybe (List Int)
+  , bearer : Encode.Value
+  }
+
+
+init : Flags -> Url -> Key -> ( Model, Cmd Msg )
+init flags origin navigationKey =
+    let
+        maybeBytes = flags.bytes |> Maybe.map convertBytes
+        maybeBearer = Debug.log "Initial Bearer" flags.bearer
+        redirectUri = { origin | query = Nothing, fragment = Nothing }
+        clearUrl = Navigation.replaceUrl navigationKey (Url.toString redirectUri)
     in
     case parseToken origin of
         OAuth.Empty ->
-            ( { flow = Idle, redirectUri = redirectUri }
-            , Cmd.none
-            )
+            withStorageToken
+                { flow = Idle, rootUrl = redirectUri }
+                ((Decode.decodeValue userFromStorageDecoder maybeBearer))
 
-        -- It is important to set a `state` when making the authorization request
-        -- and to verify it after the redirection. The state can be anything but its primary
-        -- usage is to prevent cross-site request forgery; at minima, it should be a short,
-        -- non-guessable string, generated on the fly.
-        --
-        -- We remember any previously generated state  state using the browser's local storage
-        -- and give it back (if present) to the elm application upon start
         OAuth.Success { accessToken, state, idToken } ->
-            case mflags of
+            case maybeBytes of
                 Nothing ->
-                    ( { flow = Errored ErrStateMismatch, redirectUri = redirectUri }
+                    ( { flow = Erred ErrStateMismatch, rootUrl = redirectUri }
                     , clearUrl
                     )
 
-                Just flags ->
-                    if state /= Just flags.state || idToken.parsed.nonce /= flags.state then
-                        ( { flow = Errored ErrStateMismatch, redirectUri = redirectUri }
+                Just bytes ->
+                    if state /= Just bytes.state || idToken.parsed.nonce /= bytes.state then
+                        ( { flow = Erred ErrStateMismatch, rootUrl = redirectUri }
                         , clearUrl
                         )
 
                     else
-                        ( { flow = Authorized accessToken idToken, redirectUri = redirectUri }
-                        , clearUrl
+                        ( { flow = GoogleAuthed accessToken idToken, rootUrl = redirectUri }
+                        , Cmd.batch
+                            [ after 500 RequestOtoBearer
+                            , clearUrl
+                            ]
                         )
 
         OAuth.Error error ->
-            ( { flow = Errored <| ErrAuthorization error, redirectUri = redirectUri }
+            ( { flow = Erred <| ErrAuthorization error, rootUrl = redirectUri }
             , clearUrl
             )
 
@@ -130,6 +157,7 @@ parseToken url_ =
 
         googleToken : String -> Result Jwt.JwtError JwtToken
         googleToken = decodeToken googleJWT
+        authorizationSuccessParser : Token -> IdToken -> Query.Parser AuthorizationSuccess
         authorizationSuccessParser accessToken idToken =
             Query.map3 (AuthorizationSuccess accessToken idToken Nothing)
                 (Query.int "expires_in")
@@ -195,35 +223,18 @@ type Msg
     = NoOp
     | SignInRequested
     | GotRandomBytes (List Int)
+    | RequestOtoBearer
+    | GotOtoUser (Result Http.Error OtoUser)
+    | GotOtoUserFromStorage (Result Decode.Error (Maybe OtoUser))
     | SignOutRequested
-
---getUserInfo : Configuration -> OAuth.Token -> Cmd Msg
---getUserInfo { userInfoDecoder, userInfoEndpoint } token =
---    Http.request
---        { method = "GET"
---        , body = Http.emptyBody
---        , headers = OAuth.useToken token []
---        , url = Url.toString userInfoEndpoint
---        , expect = Http.expectJson GotUserInfo userInfoDecoder
---        , timeout = Nothing
---        , tracker = Nothing
---        }
-
-
-{- On the JavaScript's side, we have:
-   app.ports.genRandomBytes.subscribe(n => {
-     const buffer = new Uint8Array(n);
-     crypto.getRandomValues(buffer);
-     const bytes = Array.from(buffer);
-     localStorage.setItem("bytes", bytes);
-     app.ports.randomBytes.send(bytes);
-   });
--}
-
 
 port genRandomBytes : Int -> Cmd msg
 
 port randomBytes : (List Int -> msg) -> Sub msg
+
+port storeSession : Maybe Decode.Value -> Cmd msg
+
+port onSessionChange : (Encode.Value -> msg) -> Sub msg
 
 --
 -- Update
@@ -232,18 +243,54 @@ port randomBytes : (List Int -> msg) -> Sub msg
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case ( model.flow, msg ) of
+        (_, NoOp) ->
+            noOp model
+
+        ( _, GotOtoUser userinfo ) ->
+            let
+                deb = Debug.log "GotOtoUser" userinfo
+            in
+                case userinfo of
+                    Ok user ->
+                        ( model, storeSession (Just (encodeUser user)) )
+                    Err error ->
+                        ( { model | flow = Erred ErrHTTPGetUserInfo}, Cmd.none )
+
+        ( _, GotOtoUserFromStorage resultMaybeOtoUser ) ->
+            withStorageToken model resultMaybeOtoUser
+
         ( Idle, SignInRequested ) ->
             signInRequested model
 
         ( Idle, GotRandomBytes bytes ) ->
             gotRandomBytes model bytes
 
-        ( Authorized token mbIdToken, SignOutRequested ) ->
-            signOutRequested model
+        ( Idle, _ ) -> noOp model
+        ( GoogleAuthed _ _, SignInRequested ) -> noOp model
+        ( GoogleAuthed _ _, GotRandomBytes _ ) -> noOp model
 
-        _ ->
+        ( GoogleAuthed accessToken idToken, RequestOtoBearer ) ->
+            requestOtoBearer model accessToken idToken
+
+        (_, SignOutRequested) ->
+            signOutRequested model
+        ( OtoAuthed _, _ ) ->
+            noOp model
+        ( Erred _, _) ->
             noOp model
 
+withStorageToken : Model -> Result Decode.Error (Maybe OtoUser) -> ( Model, Cmd Msg )
+withStorageToken model resultMaybeOtoUser =
+    case resultMaybeOtoUser of
+        Ok maybeOtoUser ->
+            case maybeOtoUser of
+                Nothing ->
+                    ( { model | flow = Idle }, Cmd.none )
+                Just user ->
+                    ( { model | flow = OtoAuthed user }, Cmd.none )
+
+        Err error ->
+            ( { model | flow = Erred (ErrCacheParse error) }, Cmd.none )
 
 noOp : Model -> ( Model, Cmd Msg )
 noOp model =
@@ -256,6 +303,106 @@ signInRequested model =
     , genRandomBytes 16
     )
 
+requestOtoBearer : Model -> OAuth.Token -> IdToken -> ( Model, Cmd Msg )
+requestOtoBearer model accessToken idToken =
+    ( { model | flow = GoogleAuthed accessToken idToken }
+    , getOtoBearer idToken
+    )
+
+getOtoBearer : IdToken -> Cmd Msg
+getOtoBearer token =
+    Http.request
+        { method = "POST"
+        , body = Http.jsonBody (encodeToken token)
+        , headers = [(Http.header "Accept" "application/json")]
+        , url = Url.toString configuration.userInfoEndpoint
+        , expect = expectJsonWithHeader GotOtoUser userInfoDecoder OtoUser
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
+
+expectJsonWithHeader : (Result Http.Error value -> msg) -> Decode.Decoder a -> (String -> a -> value) -> Expect msg
+expectJsonWithHeader toMsg decoder combiner =
+  expectStringResponse toMsg <|
+    \response ->
+      case response of
+        Http.BadUrl_ url ->
+          Err (Http.BadUrl url)
+
+        Http.Timeout_ ->
+          Err Http.Timeout
+
+        Http.NetworkError_ ->
+          Err Http.NetworkError
+
+        Http.BadStatus_ metadata body ->
+          Err (Http.BadStatus metadata.statusCode)
+
+        Http.GoodStatus_ metadata body ->
+          case Decode.decodeString decoder body of
+            Ok value ->
+                case Dict.get "authorization" metadata.headers of
+                    Just bearer -> Ok (combiner bearer value)
+                    Nothing -> Err (BadBody "No Authorization token")
+            Err err ->
+              Err (BadBody (Decode.errorToString err))
+
+
+
+type alias OtoUser =
+    { bearer : String
+    , info : OtoUserInfo
+    }
+
+type alias OtoUserInfo =
+    { email : String
+    , uid : String
+    , avatar : String
+    , name : String
+    }
+
+decodeUser : Decode.Decoder OtoUser
+decodeUser =
+    Decode.map2 OtoUser
+        (field "bearer" Decode.string)
+        (field "info" decodeUserInfo)
+
+decodeUserInfo : Decode.Decoder OtoUserInfo
+decodeUserInfo =
+    Decode.map4 OtoUserInfo
+        (field "email" Decode.string)
+        (field "uid" Decode.string)
+        (field "avatar" Decode.string)
+        (field "name" Decode.string)
+
+encodeUser : OtoUser -> Encode.Value
+encodeUser user =
+    Encode.object
+        [ ("bearer", Encode.string user.bearer)
+        , ("info", encodeUserInfo user.info)
+        ]
+
+encodeUserInfo : OtoUserInfo -> Encode.Value
+encodeUserInfo userInfo =
+    Encode.object
+        [ ("email", Encode.string userInfo.email)
+        , ("uid", Encode.string userInfo.uid)
+        , ("avatar", Encode.string userInfo.avatar)
+        , ("name", Encode.string userInfo.name)
+        ]
+
+encodeToken : IdToken -> Encode.Value
+encodeToken { raw } =
+    Encode.object [("jwt", Encode.string raw)]
+
+userInfoDecoder : Decode.Decoder OtoUserInfo
+userInfoDecoder =
+        Decode.map4 OtoUserInfo
+            (at ["data", "email"] Decode.string)
+            (at ["data", "uid"] Decode.string)
+            (at ["data", "avatar"] Decode.string)
+            (at ["data", "name"] Decode.string)
 
 gotRandomBytes : Model -> List Int -> ( Model, Cmd Msg )
 gotRandomBytes model bytes =
@@ -265,7 +412,7 @@ gotRandomBytes model bytes =
 
         authorization =
             { clientId = configuration.clientId
-            , redirectUri = model.redirectUri
+            , redirectUri = model.rootUrl
             , scope = configuration.scope
             , state = Just state
             , url = configuration.authorizationEndpoint
@@ -280,9 +427,7 @@ gotRandomBytes model bytes =
 
 signOutRequested : Model -> ( Model, Cmd Msg )
 signOutRequested model =
-    ( { model | flow = Idle }
-    , Navigation.load (Url.toString model.redirectUri)
-    )
+    ( { model | flow = Idle }, storeSession Nothing )
 
 
 -- ---------------------------
@@ -318,20 +463,49 @@ gameView : Model -> Html Msg
 gameView model =
     case model.flow of
         Idle -> signInView
-        Authorized token mbIdToken ->
-            div [][ div [] [ text (tokenToString token)]
-            , div [] [ text (mbIdToken.parsed.email)]
-            , div [] [ button
-                        [ onClick SignOutRequested, class "" ]
-                        [ text "Sign out" ]
-                    ]
+        Erred error -> errorView error
+        _ -> authorizedView model
+
+
+authorizedView : Model -> Html Msg
+authorizedView model =
+    div [ class "flex flex-col m-10 justify-center items-center"]
+        [ googleCredsView model.flow
+        , div [] [ button
+                    [ onClick SignOutRequested, class "" ]
+                    [ text "Sign out" ]
+                ]
+        ]
+
+
+googleCredsView : Flow -> Html msg
+googleCredsView flow =
+    let
+        img_ =
+            case flow of
+                GoogleAuthed _ idToken -> img [ attribute "referrerpolicy" "no-referrer", class "w-14 h-14 rounded-full", src (idToken.parsed.picture)] []
+                OtoAuthed otoUser -> img [ attribute "referrerpolicy" "no-referrer", class "w-14 h-14 rounded-full", src (otoUser.info.avatar)] []
+                _ -> text ""
+        info_ =
+            case flow of
+                GoogleAuthed _ idToken -> span [ class "pl-4 overflow-ellipsis overflow-hidden"] [text (idToken.parsed.email)]
+                OtoAuthed otoUser ->  span [ class "pl-4 overflow-ellipsis overflow-hidden"] [text (otoUser.info.name)]
+                _ -> text ""
+    in
+        div [ class "transition-transform transform  w-full md:w-1/2 surface-1 on-surface-text rounded-lg flex flex-row p-4 mb-8 text-lg shadow-md justify-left items-center" ]
+            [ img_
+            , info_
             ]
-        Errored error -> div [ class "flex" ]
-                             [ div
-                                       [ class "step", class "step-errored" ]
-                                       [ span [ style "left" "-50%" ] [ text "Errored" ]
-                                       , span [ class "span-error" ] [ viewError error ] ]
-                                       ]
+
+
+errorView : Error -> Html Msg
+errorView error =
+    div [ class "flex m-10 justify-center items-center"]
+        [ div [ class "w-full md:w-1/2 error-container on-error-container-text rounded-lg flex flex-row p-4 mb-8 text-sm shadow-md focus:outline-none focus:shadow-outline-purple" ]
+            [ span [class "material-icons md-18"] [ text "error" ]
+            , span [ class "ml-3 w-full overflow-ellipsis overflow-hidden" ] [ viewError error ]
+            ]
+        ]
 
 signInView : Html Msg
 signInView =
@@ -364,21 +538,12 @@ viewError e =
             ErrHTTPGetUserInfo ->
                 "Unable to retrieve user info: HTTP request failed."
 
--- ---------------------------
--- MAIN
--- ---------------------------
+            ErrCacheParse error ->
+                let
+                    a = Debug.log "Some problems with localStorage parsing" (Decode.errorToString error)
+                in
+                 "Some weird unexpected error"
 
-
-main : Program (Maybe (List Int)) Model Msg
-main =
-    application
-        { init = Maybe.map convertBytes >> init
-        , update = update
-        , subscriptions = always <| randomBytes GotRandomBytes
-        , onUrlRequest = always NoOp
-        , onUrlChange = always NoOp
-        , view = view
-        }
 
 
 view : Model -> Document Msg
@@ -432,6 +597,16 @@ oauthErrorToString { error, errorDescription } =
 defaultHttpsUrl : Url
 defaultHttpsUrl =
     { protocol = Https
+    , host = ""
+    , path = ""
+    , port_ = Nothing
+    , query = Nothing
+    , fragment = Nothing
+    }
+
+defaultHttpUrl : Url
+defaultHttpUrl =
+    { protocol = Http
     , host = ""
     , path = ""
     , port_ = Nothing
